003b97a213d4e50d444685b7b6b6afc4fc47b832
Carlos Adir <carlos.adir.leite@gmail.com>
2023-01-14 22:38:50 -0300

    Separar files do trabalho 3

--- mntf/helper.py
+++ mntf/helper.py
@@ -1,11 +1,12 @@
 from compmec import nurbs
 import math
 import numpy as np
-from typing import Callable, Tuple
+from tqdm import tqdm
+from typing import Callable, Tuple, Optional
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from numpy.random import default_rng
-
+np.set_printoptions(4)
 
 
 def getMatrix(y: int, z: int, k: int, N: nurbs.SplineBaseFunction):
@@ -95,14 +96,14 @@ def rungekutta(f: Callable[[float, float], float], y0: float, t: Tuple[float]):
         w_[i + 1] = f(t[i + 1], w[i + 1])
     return w
 
-def plot_field(xmesh: Tuple[float], ymesh: Tuple[float], zvals: np.ndarray, ax = None):
+def plot_field(xmesh: Tuple[float], ymesh: Tuple[float], zvals: np.ndarray, ax = None, contour=True):
     xmesh = np.array(xmesh, dtype="float64")
     ymesh = np.array(ymesh, dtype="float64")
     zvals = np.array(zvals, dtype="float64")
     assert xmesh.ndim == 1
     assert ymesh.ndim == 1
     assert zvals.ndim == 2
-    assert zvals.shape == (len(ymesh), len(xmesh))
+    assert zvals.shape == (len(xmesh), len(ymesh))
     x, y = np.meshgrid(xmesh, ymesh)
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 10))
@@ -111,120 +112,290 @@ def plot_field(xmesh: Tuple[float], ymesh: Tuple[float], zvals: np.ndarray, ax =
     dx = 0.05*(xmesh[-1]-xmesh[0])/2.
     dy = 0.05*(ymesh[-1]-ymesh[0])/2.
     extent = [xmesh[0]-dx, xmesh[-1]+dx, ymesh[0]-dy, ymesh[-1]+dy]
-    im = ax.imshow(zvals[::-1], cmap="viridis", interpolation='nearest', aspect='auto', extent=extent)
-    cp = ax.contour(x, y, zvals, 10, colors="k")
+    im = ax.imshow((zvals.T)[::-1], cmap="viridis", interpolation='nearest', aspect='auto', extent=extent)
+    if contour:
+        cp = ax.contour(x, y, zvals.T, 10, colors="k")
     div  = make_axes_locatable(ax)
     cax  = div.append_axes('bottom', size='5%', pad=0.6)
     cbar = plt.colorbar(im, cax=cax, orientation='horizontal')
     return ax
     
 
-def solve_system(A: np.ndarray, B: np.ndarray, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def solve_system(A: np.ndarray, B: np.ndarray, X: np.ndarray, X0: Optional[np.ndarray] = None) -> Tuple[np.ndarray, np.ndarray]:
     """
     Resolve o sistema A * X = B
     Recebe 
         ```A```: Matriz de tamanho (n1, n2, n1, n2)
         ```B```: Matriz de tamanho (n1, n2)
-        ```X```: Matriz de tamanho (n1, n2), com valores None dentro, com condicoes de contorno
+        ```X```: Matriz de tamanho (n1, n2), com valores ```nan``` dentro e com condicoes de contorno
+    Se X0 for dado, eh usado um metodo iterativo
     """
+    
+    if B.shape != X.shape:
+        raise ValueError(f"B.shape = {B.shape} != {X.shape} = X.shape")
+    if X0 is not None:
+        if X0.shape != X.shape:
+            raise ValueError(f"X0.shape = {X0.shape} != {X.shape} = X.shape")
+    if A.ndim != 2*B.ndim:
+        raise ValueError("A.ndim = {A.ndim} != 2*{B.ndim} = 2*B.ndim")
+    if not np.any(np.isnan(X)):
+        raise ValueError("At least one unknown must be given! All values of X are known")
+
+    Xoriginal = np.copy(X)
     ns = B.shape
     ndim = len(ns)
     ntot = np.prod(ns)
-    mexp = (X == None).flatten()
     Aexp = np.zeros((ntot, ntot), dtype="float64")
     Bexp = np.zeros(ntot, dtype="float64")
-    Xexp = np.zeros(ntot, dtype="object")
     if ndim == 1:
-        Aexp[:, :] = A[:, :]
-        Bexp[:] = B[:]
-        Xexp[:] = X[:]
+        Aexp = np.copy(A)
+        Bexp = np.copy(B)
+        Xexp = np.copy(X)
+        if X0 is not None:
+            X0exp = np.copy(X0)
     elif ndim == 2:
-        inter0 = 0
-        for x in range(ns[0]):
-            for y in range(ns[1]):
-                Bexp[inter0] = B[x, y]
-                Xexp[inter0] = X[x, y]
-                inter1 = 0
-                for i in range(ns[0]):
-                    for j in range(ns[1]):
-                        Aexp[inter0, inter1] = A[x, i, y, j]
-                        inter1 += 1
-                inter0 += 1
+        Aexp = np.zeros((ntot, ntot), dtype="float64")
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                Aexp[i*ns[1]+j, :] = A[i, :, j, :].reshape(ntot)
+        Bexp = np.copy(B).reshape(ntot)
+        Xexp = np.copy(X).reshape(ntot)
+        if X0 is not None:
+            X0exp = np.copy(X0).reshape(ntot)
     elif ndim == 3:
-        inter0 = 0
-        for x in range(ns[0]):
-            for y in range(ns[1]):
-                for z in range(ns[2]):
-                    Bexp[inter0] = B[x, y, z]
-                    Xexp[inter0] = X[x, y, z]
-                    inter1 = 0
-                    for i in range(ns[0]):
-                        for j in range(ns[1]):
-                            for k in range(ns[2]):
-                                Aexp[inter0, inter1] = A[x, i, y, j, z, k]
-                                inter1 += 1
-                    inter0 += 1
-    mexp = (Xexp == None)
+        Aexp = np.zeros((ntot, ntot), dtype="float64")
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                for k in range(ns[2]):
+                    Aexp[i*ns[2]*ns[1]+j*ns[2]+k, :] = A[i, :, j, :, k].reshape(ntot)
+        Bexp = np.copy(B).reshape(ntot)
+        Xexp = np.copy(X).reshape(ntot)
+        if X0 is not None:
+            X0exp = np.copy(X0).reshape(ntot)
+    mexp = np.isnan(Xexp)
     Auu = np.delete(np.delete(Aexp, ~mexp, axis=0), ~mexp, axis=1)
     Aku = np.delete(np.delete(Aexp, mexp, axis=0), ~mexp, axis=1)
     Auk = np.delete(np.delete(Aexp, ~mexp, axis=0), mexp, axis=1)
     Akk = np.delete(np.delete(Aexp, mexp, axis=0), mexp, axis=1)
-    Xexp[mexp] = np.linalg.solve(Auu, (Bexp[mexp] - Auk @ Xexp[~mexp]).astype("float64"))
+    newvec = Bexp[mexp] - (Auk @ Xexp[~mexp].astype("float64"))
+    if X0 is None:
+        Xexp[mexp] = np.linalg.solve(Auu, newvec)  # Resolvemos diretamente
+    else:
+        Xexp[mexp], _ = GradienteConjugado(Auu, newvec, X0exp[mexp])
+        # Xexp[mexp], _ = GaussSeidel(Auu, newvec, X0exp[mexp])
+
     Bexp[~mexp] = Aku @ Xexp[mexp] + Akk @ Xexp[~mexp]
     X = Xexp.reshape(ns)
     B = Bexp.reshape(ns)
     return X, B
 
+def GaussSeidel(A: np.ndarray, B: np.ndarray, X0: np.ndarray, atol: float = 1e-9, verbose = False) -> Tuple[np.ndarray, int]:
+    n = len(B)
+    iteration = 0
+    itermax = 200
+    Xnew = np.copy(X0)
+    while True:
+        if verbose:
+            print(f"X[{iteration}] = ", Xnew)
+        for i in range(n):
+            Xnew[i] = B[i]
+            Xnew[i] -= sum(A[i,:i]*Xnew[:i])
+            Xnew[i] -= sum(A[i,i+1:]*X0[i+1:])
+            Xnew[i] /= A[i, i]
+        error = np.max(np.abs(Xnew-X0))
+        if verbose:
+            print("    error = %.2e" % error)
+        if error < atol:
+            return Xnew, iteration
+        X0 = np.copy(Xnew)
+        iteration += 1
+        if iteration > itermax:
+            error_msg = f"Gauss Seidel doesn't converge."
+            raise ValueError(error_msg)
+
+def GradienteConjugado(A: np.ndarray, B: np.ndarray, X0: np.ndarray, atol: float = 1e-9, itermax: int = 200, verbose=False) -> Tuple[np.ndarray, int]:
+    atol *= len(X0)
+    iteration = 0
+    r0 = B - A @ X0
+    residuo = np.max(np.abs(r0))
+    if residuo < atol:
+        return X0, 0
+    rnew = np.copy(r0)
+    p0 = np.copy(r0)
+    Xnew = np.copy(X0)
+    while True:
+        if verbose:
+            print(f"X[{iteration}] = ", Xnew)
+        alpha = np.inner(r0, r0)
+        alpha /= (p0 @ A @ p0)
+        Xnew[:] = X0[:] + alpha * p0[:]
+        rnew[:] = r0[:] - alpha * A @ p0
+        error = np.max(np.abs(Xnew-X0))
+        residuo = np.max(np.abs(rnew))
+        if verbose:
+            print("    error = %.2e" % error)
+            print("    resid = %.2e" % residuo)
+        if residuo < atol:
+            return Xnew, iteration
+        beta = np.inner(rnew, rnew)
+        beta /= np.inner(r0, r0)
+        p0 *= beta
+        p0[:] += rnew[:]
+        r0[:] = rnew[:]
+        iteration += 1
+        if iteration > itermax:
+            error_msg = f"Gradiente conjugado doesn't converge."
+            raise ValueError(error_msg)
+
+def get_random_matrix_definite_positive(side: int):
+    A = np.random.rand(side, side)
+    A += np.transpose(A)
+    eig, P = np.linalg.eig(A)
+    P = np.real(P)
+    eig = 2+np.random.rand(side)
+    A = P.T @ np.diag(eig) @ P
+    for i in range(side):
+        A[i, i] = np.sum(np.abs(A[i]))+0.01
+        A[i] /= 2*A[i, i]
+    A += np.transpose(A)
+    eigs, _ = np.linalg.eigh(A)
+    assert np.all(eigs > 0)
+    assert np.all(A == np.transpose(A))
+    return A
 
 if __name__ == "__main__":
-    n = 5
+    aproximacao = True
 
-    A = np.random.rand(n, n)
-    X0 = np.random.rand(n)
-    B = np.einsum("ij,j->i", A, X0)
-    BCs = np.zeros(n, dtype="bool")
-    allchoices = [a for a in range(n)]
-    numbers = default_rng().choice(allchoices, size=np.random.randint(1, n), replace=False)
-    for i in numbers:
-        BCs[i] = True
-    Xt = np.empty(n, dtype="object")
-    Xt[BCs] = X0[BCs]
-    X1, _ = solve_system(A, B, Xt)
-    np.testing.assert_almost_equal(X0, X1)
+    ntests = 200
+    for kkk in tqdm(range(ntests)):
+        n1 = np.random.randint(4, 9)
+        A = get_random_matrix_definite_positive(n1)
+        Xgood = np.random.rand(n1)
+        B = np.einsum("ij,j->i", A, Xgood)
+        BCs = np.zeros(n1, dtype="bool")
+        allchoices = [a for a in range(n1)]
+        numbers = default_rng().choice(allchoices, size=np.random.randint(1, n1), replace=False)
+        for i in numbers:
+            BCs[i] = True
+        Xboundary = np.empty(n1)
+        Xboundary.fill(np.nan)
+        Xboundary[BCs] = Xgood[BCs]
+        if aproximacao:
+            Xinit = Xgood+0.01*np.random.rand(n1)
+            Xinit[BCs] = Xgood[BCs]
+            X1, _ = solve_system(A, B, Xboundary, Xinit)
+        else:
+            X1, _ = solve_system(A, B, Xboundary)
+        mexp = np.isnan(Xboundary)
+        np.testing.assert_almost_equal(X1[~mexp], Xgood[~mexp])
+        np.testing.assert_almost_equal(X1[mexp], Xgood[mexp])
 
-    n1, n2 = 4, 5
-    A = np.random.rand(n1, n1, n2, n2)
-    X0 = np.random.rand(n1, n2)
-    B = np.einsum("xiyj,ij->xy", A, X0)
-    BCs = np.zeros((n1, n2), dtype="bool")
-    allchoices = []
-    for a in range(n1):
-        for b in range(n2):
-            allchoices.append((a, b))
-    numbers = default_rng().choice(allchoices, size=np.random.randint(1, n1*n2), replace=False)
-    for i, j in numbers:
-        BCs[i, j] = True
-    Xt = np.empty((n1, n2), dtype="object")
-    Xt[BCs] = X0[BCs]
-    X1, _ = solve_system(A, B, Xt)
-    np.testing.assert_almost_equal(X0, X1)
+    for kkk in tqdm(range(ntests)):
+        ns = np.random.randint(4, 9, size=2)
+        ntot = np.prod(ns)
 
-    n1, n2, n3 = 4, 5, 7
-    A = np.random.rand(n1, n1, n2, n2, n3, n3)
-    X0 = np.random.rand(n1, n2, n3)
-    B = np.einsum("xiyjzk,ijk->xyz", A, X0)
-    BCs = np.zeros((n1, n2, n3), dtype="bool")
-    allchoices = []
-    for a in range(n1):
-        for b in range(n2):
-            for c in range(n3):
-                allchoices.append((a, b, c))
-    numbers = default_rng().choice(allchoices, size=np.random.randint(1, n1*n2*n3), replace=False)
-    for i, j, k in numbers:
-        BCs[i, j, k] = True
-    Xt = np.empty((n1, n2, n3), dtype="object")
-    Xt[BCs] = X0[BCs]
-    X1, _ = solve_system(A, B, Xt)
-    np.testing.assert_almost_equal(X0, X1)
+        BCs = np.zeros(ns, dtype="bool")
+        allchoices = []
+        for a in range(ns[0]):
+            for b in range(ns[1]):
+                allchoices.append((a, b))
+        numbers = default_rng().choice(allchoices, size=np.random.randint(1, ntot), replace=False)
+        for i, j in numbers:
+            BCs[i, j] = True
+        
+        Aexp = get_random_matrix_definite_positive(ntot)
+        Aexp = np.around(Aexp, 3)
+        A = np.zeros((ns[0], ns[0], ns[1], ns[1]), dtype="float64")
+        Xgood = np.around(np.random.rand(*ns), 1)
+        B = np.zeros((ns[0], ns[1]), dtype="float64")
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                A[i, :, j, :] = Aexp[i*ns[1]+j, :].reshape(ns)
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                B[i, j] += np.tensordot(A[i, :, j, :], Xgood, axes=2)
+        Xexpgood = np.zeros((ntot), dtype="float64")
+        Bexp = np.zeros(ntot, dtype="float64")
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                Xexpgood[i*ns[1]+j] = Xgood[i, j]
+                Bexp[i*ns[1]+j] = B[i, j]
+        
+        Xboundary = np.empty(ns)
+        Xboundary.fill(np.nan)
+        Xboundary[BCs] = Xgood[BCs]
+
+        Aexptest = np.zeros((ntot, ntot), dtype="float64")
+        Bexptest = B.reshape(ntot)
+        np.testing.assert_almost_equal(Bexptest, Bexp)
+        
+        assert np.all( np.abs(Aexp @ Xexpgood - Bexp) < 1e-6)
+
+        if aproximacao:
+            Xinit = Xgood+0.01*np.random.rand(*ns)
+            Xinit[BCs] = Xboundary[BCs]
+            X1, _ = solve_system(A, B, Xboundary, Xinit)
+        else:
+            X1, _ = solve_system(A, B, Xboundary)
+        
+        mexp = np.isnan(Xboundary)
+        np.testing.assert_almost_equal(X1[~mexp], Xgood[~mexp])
+        np.testing.assert_almost_equal(X1[mexp], Xgood[mexp])
+        # np.testing.assert_almost_equal(X1, Xgood)
+
+    for kkk in tqdm(range(ntests)):
+        ns = np.random.randint(4, 9, size=3)
+        ntot = np.prod(ns)
+
+        BCs = np.zeros(ns, dtype="bool")
+        allchoices = []
+        for a in range(ns[0]):
+            for b in range(ns[1]):
+                for c in range(ns[2]):
+                    allchoices.append((a, b, c))
+        numbers = default_rng().choice(allchoices, size=np.random.randint(1, ntot), replace=False)
+        for i, j, k in numbers:
+            BCs[i, j, k] = True
+        
+        Aexp = get_random_matrix_definite_positive(ntot)
+        Aexp = np.around(Aexp, 3)
+        A = np.zeros((ns[0], ns[0], ns[1], ns[1], ns[2], ns[2]), dtype="float64")
+        Xgood = np.around(np.random.rand(*ns), 1)
+        B = np.zeros(ns, dtype="float64")
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                for k in range(ns[2]):
+                    A[i, :, j, :, k, :] = Aexp[i*ns[1]*ns[2]+j*ns[2]+k, :].reshape(ns)
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                for k in range(ns[2]):
+                    B[i, j, k] += np.tensordot(A[i, :, j, :, k, :], Xgood, axes=3)
+        Xexpgood = np.zeros((ntot), dtype="float64")
+        Bexp = np.zeros(ntot, dtype="float64")
+        for i in range(ns[0]):
+            for j in range(ns[1]):
+                for k in range(ns[2]):
+                    Xexpgood[i*ns[1]*ns[2]+j*ns[2]+k] = Xgood[i, j, k]
+                    Bexp[i*ns[1]*ns[2]+j*ns[2]+k] = B[i, j, k]
+        
+        Xboundary = np.empty(ns)
+        Xboundary.fill(np.nan)
+        Xboundary[BCs] = Xgood[BCs]
+
+        Aexptest = np.zeros((ntot, ntot), dtype="float64")
+        Bexptest = B.reshape(ntot)
+        np.testing.assert_almost_equal(Bexptest, Bexp)
+        
+        assert np.all( np.abs(Aexp @ Xexpgood - Bexp) < 1e-6)
+
+        if aproximacao:
+            Xinit = Xgood+0.1*np.random.rand(*ns)
+            Xinit[BCs] = Xboundary[BCs]
+            X1, _ = solve_system(A, B, Xboundary, Xinit)
+        else:
+            X1, _ = solve_system(A, B, Xboundary)
+        mexp = np.isnan(Xboundary)
+        np.testing.assert_almost_equal(X1[~mexp], Xgood[~mexp])
+        np.testing.assert_almost_equal(X1[mexp], Xgood[mexp])
 
 
